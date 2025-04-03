c5b1563ed9681fc559dd728e218078f4b0667c59
Evan Kweder <ekweder@icloud.com>
2025-04-03 17:21:00 -0600

    Test cases

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -24,6 +24,4 @@ app.get('/login', (req, res) => {
     res.render('pages/login');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+module.exports = app.listen(3000);
