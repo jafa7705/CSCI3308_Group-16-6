ad076a8b7abaf5f7f55c1a4f1d34b97d73f9dabe
Jiaye Zheng <JustinZheng0518@gmail.com>
2025-04-04 14:19:27 -0600

    done the setup of the database

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(200) NOT NULL,
+  email VARCHAR(200) NOT NULL UNIQUE,
+  role VARCHAR(20) NOT NULL CHECK (role IN ('artist', 'client')),
+  bio TEXT,
+  website VARCHAR(200),
+  location VARCHAR(100),
+  phone VARCHAR(20),
+);
+
+DROP TABLE IF EXISTS artworks;
+CREATE TABLE artworks (
+  artwork_id SERIAL PRIMARY KEY,
+  artist_id INTEGER NOT NULL REFERENCES users (user_id),
+  title VARCHAR(100) NOT NULL,
+  creation_date DATE NOT NULL,
+  description TEXT,
+  -- TODO: Add a key to store the image and the text.
+  category VARCHAR(50) NOT NULL
+);
