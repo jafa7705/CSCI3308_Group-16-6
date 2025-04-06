9cd2f7c5078991951a231352b6dc8f68b8995df4
Christian Mesa <chme9651@colorado.edu>
2025-04-05 20:42:49 -0600

    Contact me and edit button visiblity works, edited db for isClient, added test profiles

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -31,9 +31,21 @@ app.get('/', (req, res) => {
 });
 
 
+// Profile ROutes
+app.get('/profile', async (req, res) => {
+  const userID = 3; //TODO: Replace with login stuff
+  const profileUserID = 1; // TODO: replace with the profile ID on the page
 
-app.get('/profile', (req, res) => {
-  res.render('pages/profile');
+  try {
+      const user = await db.one('SELECT user_id, username, email, isClient, bio, website, location FROM users WHERE user_id = $1', [profileUserID]);
+      const isOwner = userID === profileUserID;
+      const isOwnerOrClient = isOwner || (await db.one('SELECT isClient FROM users WHERE user_id = $1', [userID])).isClient;
+
+      res.render('pages/profile', { user, isOwner, isOwnerOrClient });
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('ERROR: Could not get profile');
+  }
 });
 
 
